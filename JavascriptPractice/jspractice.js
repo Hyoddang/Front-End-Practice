19a6756eb4b259cd0c8738fd282da8e57676eea3
Hyoddang <yhd5689@naver.com>
2023-06-14 21:50:16 +0900

    06월 14일 까지 공부한 자료

---
+++ JavascriptPractice/jspractice.js
@@ -0,0 +1,25 @@
+// var id = prompt('아이디를 입력 해주세요.');
+
+// if (id === 'doit159') {
+//     var password = prompt('비밀번호를 입력 해주세요.'); 
+
+//         if (password === 'dbstjdgy1!@') {
+//             alert('반갑습니다. '+id+'님');
+//         } else {
+//         alert('로그인에 실패하셨습니다. 비밀번호를 확인 해주세요.');
+//     } 
+
+// } else {
+//     alert ('로그인에 실패 하셨습니다. 아이디를 확인 해주세요.');
+//     }
+
+// var a = 0;
+// while (a < 10) {
+//     document.write ("안녕하세요. <br />");
+//     a = a + 1;
+// }
+
+
+for (var i = 0; i < 100; i++) {
+    document.write("안녕하세요. <br />");
+}
